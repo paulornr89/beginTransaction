7c6fcdcbdcb5cfec6109de6de5b32623092bc4f1
paulornr89 <paulornr89@gmail.com>
2024-10-16 09:38:47 -0300

    funcionando a transaction

--- index.js
+++ index.js
@@ -6,15 +6,36 @@ const connect = async () => {
     try {
         // make sure that any items are correctly URL encoded in the connection string
         //await sql.connect('Server=192.168.7.232,1433;Database=dadostst7;User Id=paulo;Password=Pp@paulo2021;Encrypt=true')
+        console.log(this.room)
         await sql.connect(config())
         console.log("conectou?")
-        console.log(sql)
-        const result = await sql.query("SELECT * FROM ZCA010 WHERE ZCA_MAT = '003136'")
-        console.dir(result)
+        // console.log(this.room.db)
+        //console.log(sql)
+        // const result = await sql.query("SELECT * FROM ZCA010 WHERE ZCA_MAT = '003136'")
+        //console.dir(result)
+        const transaction = new sql.Transaction()
+
+        transaction.begin(err => {
+            const request = new sql.Request(transaction)
+            request.query(`
+                INSERT INTO ZCA010 (ZCA_FILIAL, ZCA_MAT, ZCA_CARGO, ZCA_CAPACI, ZCA_INICIO, ZCA_FIM, ZCA_HORAS, ZCA_ANOREF, ZCA_FLUIG, R_E_C_N_O_)
+                VALUES ('0101', '003136', '02077', 'TESTE - TRANSACTION', '20241009', '20241009', '14.52', '2024', '11111', (SELECT TOP 1 (R_E_C_N_O_ + 1) FROM ZCA010 ORDER BY R_E_C_N_O_ DESC))
+            `, (err, result) => {
+                // ... error checks
+                console.log("Resultado")
+                console.log(result)
+                transaction.commit(err => {
+                    // ... error checks
+                    console.log("Transaction committed.")
+                })
+            })
+        })
     } catch (err) {
         console.log(err)
     }
 }
+
+
 
 connect()
 function config () {
